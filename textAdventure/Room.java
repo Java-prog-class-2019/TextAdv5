f61235beb6ab2024e7d71cb0493690a45c9e24f3
Freya <mosefrey085@gotvdsb.ca>
2019-10-16 11:02:36 -0400

    Added some rooms and fixed it so you can use more direction commands

--- textAdventure/Room.java
+++ textAdventure/Room.java
@@ -17,7 +17,7 @@ class Room {
 	}
 	
 	static void setupRooms(HashMap<String,Room> roomList) {
-		Room r = new Room("A dark basement", 
+		/*Room r = new Room("A dark basement", 
 				"You are in a dark basement. \nThere is a dirty window that allows a little light in. There is a desk here.");
 		r.n = "tunnel";
 		roomList.put("basement1", r);
@@ -26,5 +26,22 @@ class Room {
 				"You are in a tunnel that twists as it goes downwards...");
 		r.s = "basement1";
 		roomList.put("tunnel", r);
+		*/
+		Room r = new Room("Your Cell",
+				"You find yourself in a dingy prison cell. The door out of your cell, which is to the north of you, is locked.");
+		r.n = ("hallway1");
+		roomList.put("cell1", r);
+		r = new Room ("Hallway", "You find yourself in a dimly lit hallway, which leads North, East, and West.");
+		r.s="cell1";
+		r.n="hallway3";
+		r.e="hallway2";
+		r.w="hallway4";
+		roomList.put("hallway1", r);
+		//hallway 2
+		r = new Room ("Hallway", "You are in a long hallway which runs East to West");
+		r.w="hallway1";
+		r.e="hallway5";
+		roomList.put("hallway2", r);
+		
 	}
 }
